81428805fc54345a9cdf2edf5d5f0a9075127612
unknown <williamsartijose@hotmail.com>
2023-06-14 08:01:55 +0100

    Rota /Report- Configurando a classe /SaleController.java

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,13 +1,17 @@
 package com.devsuperior.dsmeta.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
 import com.devsuperior.dsmeta.services.SaleService;
 
 @RestController
@@ -24,11 +28,15 @@ public class SaleController {
 	}
 
 	@GetMapping(value = "/report")
-	public ResponseEntity<?> getReport() {
+	public ResponseEntity<Page<SaleReportDTO>> getReport(
+		@RequestParam(name = "minDate", defaultValue = "") String minDate,
+		@RequestParam(name = "maxDate", defaultValue = "") String maxDate,
+		@RequestParam(name = "name", defaultValue = "") String name,
+		Pageable pageable){
+			Page<SaleReportDTO> dto = service.getReport(minDate, maxDate, name, pageable);
+			return ResponseEntity.ok(dto);
+		}
 		
-		return null;
-	}
-
 	@GetMapping(value = "/summary")
 	public ResponseEntity<?> getSummary() {
 		// TODO
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -3,9 +3,12 @@ package com.devsuperior.dsmeta.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 
@@ -19,5 +22,10 @@ public class SaleService {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
 		return new SaleMinDTO(entity);
+	}
+
+	public Page<SaleReportDTO> getReport(String minDate, String maxDate, String name, Pageable pageable) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
